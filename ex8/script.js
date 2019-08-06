24e32f4fdf0d12b6b25efbbfd4577268fc4bc698
Alvin Ng <ngalvin93@gmail.com>
2019-08-05 22:57:49 -0500

    added check off from list function

--- ex8/script.js
+++ ex8/script.js
@@ -10,16 +10,45 @@ document.querySelector('#add-button').onclick = addToList;
 function addToList () {
     let listItem = document.createElement('li');
     let taskText = document.querySelector('#description').value;
+    if (taskText.length > 0) {
     listItem.innerHTML = taskText
     document.querySelector('#todo-list').appendChild(listItem);
+    // create a x button for each new li here
+    document.querySelector('#description').value = ''; // this clears the input when the function is complete
+    } else {
+        alert("You must enter a task name!")
+    }
 }
+
+// enter key to add item to the list
+document.body.onkeyup = function (event) {
+    if (event.keyCode == 13) {
+        addToList();
+    }
+};
 
 // this marks the item as done
 let list = document.querySelector('#todo-list');
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName) {
-        ev.target.classList.add = ('complete');
-    } else {
-        console.log('this is false');
+list.addEventListener ('click', function (event) {
+    event.target.className += ('complete')
+})
+
+//this adds a delete button to each list item
+let taskListItems = document.getElementsByTagName('li');
+var i;
+for (i=0;i<taskListItems.length;i++) {
+    let span = document.createElement('span');
+    let x = document.createTextNode('x');
+    span.className = 'close';
+    span.appendChild(x);
+    taskListItems[i].appendChild(span);
+}
+//this adds the delete function
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
     }
-});
+};
